54be4aefd3cd4057bb2ce307ae72e3a9b6de30b2
Insoftar <leonardo.carreno@insoftar.com>
2022-08-05 21:37:38 -0500

    CREATE LOCATOR, FUNTION Y TEST

--- src/test/java/createProducto/createProductoPage.java
+++ src/test/java/createProducto/createProductoPage.java
@@ -1,5 +1,33 @@
 package createProducto;
 
-public class createProductoPage {
+import java.util.NoSuchElementException;
 
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.WebDriver;
+
+import Base.BasicWarap;
+
+
+public class createProductoPage extends BasicWarap {
+	
+	public createProductoPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+	public void getIntoUser(String Email, String Password) throws Exception {
+		try {
+			Thread.sleep(5000);
+			click(locatorCreateProducto.LOCATOR_GET_INTRO);
+			type(Email, locatorCreateProducto.LOCATOR_EMAIL);
+			type(Password, locatorCreateProducto.LOCATOR_PASSWORD);
+			click(locatorCreateProducto.LOCATOR_START);
+		
+		}catch (NoSuchElementException e) {
+			System.out.println("Error: "+e);
+		}catch(ElementClickInterceptedException e) {
+			System.out.println("Error: "+e);
+		}catch (Exception e) {
+			System.out.println("Error: "+e);
+		}
+	}
 }
--- src/test/java/createProducto/createProductoTest.java
+++ src/test/java/createProducto/createProductoTest.java
@@ -5,20 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import Base.BaseData;
+
+
 
 public class createProductoTest {
 
-	@Before
-	public void setUp() throws Exception {
+		private WebDriver driver;
+		createProductoPage CreateProductoPage;
+		
+
+		@Before
+		public void setUp() throws Exception {
+			CreateProductoPage = new createProductoPage(driver);
+			driver = CreateProductoPage.chromeDriverConnection();
+			CreateProductoPage.visit("https://www.productosok.com");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//driver.quit();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test()throws Exception {
+		CreateProductoPage.getIntoUser(BaseData.EMAIL,BaseData.PASSWORD);
 	}
 
 }
